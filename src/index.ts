53c948f3d6ad3149fac08ec6f18e0707a54ccf70
Filippofr <filippo.fr7@gmail.com>
2023-12-30 20:56:46 +0100

    fine api

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 
 import app from './app';
 import mongoose from 'mongoose';
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const URI = "mongodb+srv://filippofr:DatabaseSicuro99@clusterfilippo.t2ui8vk.mongodb.net/config-maserati";
+const DB_URI = process.env.DB_URI;
+
 
 mongoose.set('debug', true);
-mongoose.connect(URI)
+mongoose.connect(DB_URI!)
   .then(_ => {
     console.log('Connected to db');
     app.listen(3000, () => {
